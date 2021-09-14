f470caf1f23bb8d85b4f14f08d9277a329436122
nhungao <90708999+nhungao@users.noreply.github.com>
2021-09-14 23:01:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'con ngốn lào à!chúc sn m :vvv fak you',
+    introDesc: `họ cần 1 mối tình noel
+    tôi chỉ cần tình bạn no end :33`,
+    btnIntro: 'ò dễ thưng :33',
+    title: 'bồ công anh bay khi có gió  😙',
+    desc: 'tôi chỉ cười khi con tó tên như :))) t bue due chơi less k :)) ',
+    btnYes: 'okii lun :33 <33',
+    btnNo: 'delll và dell',
+    question:'thế gian thạt lém bộn bề sao k chơi less rồi vìa bên nheo , t đẹp trai k :))) <3',
+    btnReply: ' nói đê 
+    <3',
+    reply: ' you đẹp trai vcl choi less lun :))) <333',
+    mess: 't bít t đẹp mà m đừng khen t ngại :33 😘😘',
+    messDesc: 'sau có gì học lại t đập m nhoaa :))).',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
